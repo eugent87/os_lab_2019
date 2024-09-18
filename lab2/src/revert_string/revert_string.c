3b58c060279c2281d73bdf5715dc1d3acd8f6e62
eugent87 <127997253+eugent87@users.noreply.github.com>
2024-09-18 11:15:10 +0000

    лаб2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,23 @@
 #include "revert_string.h"
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char temp;
+	char* str2=str;
+	while(*str2!='\0')
+	{
+		str2++;
+	}
+	str2--;
+	while(str<str2)
+	{
+		temp=*str;
+		*str=*str2;
+		*str2=temp;
+		str2--;
+		str++;
+	}
+	
 }
 
